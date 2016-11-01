d3fca4a4ad227bf8938154cbd574818f5a20b275
Jonan Scheffler <jonan@heroku.com>
2016-11-01 12:06:27 -0700

    Trying to get Alexa to slow down.

--- app/models/intents/help.rb
+++ app/models/intents/help.rb
@@ -8,7 +8,7 @@ class Intents::Help
     text = "You can tell Alexa to ask Deploy Bot to deploy your applications.\n"
     text << "For example, you can say:\n"
     text << "Alexa, ask Deploy Bot to deploy steve to production.\n"
-    text << "or\n"
+    text << "or.\n"
     text << "Alexa, tell Deploy Bot to deploy hal to staging\n"
     text
   end
