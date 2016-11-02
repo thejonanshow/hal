499f1a54cfdf9799ea2f5c5e07deec57a3af9eec
Jonan Scheffler <jonan@heroku.com>
2016-11-02 06:32:04 -0700

    Periods are not dots Alexa.

--- app/models/intents/deploy.rb
+++ app/models/intents/deploy.rb
@@ -9,10 +9,11 @@ class Intents::Deploy
   end
 
   def output_text
-    text = "HAL has deployed #{application} to #{environment}, hypothetically."
+    text = "HAL has deployed #{application} to #{environment}, hypothetically. "
     text << "I'm not actually able to give you information about whether or not "
     text << "the deploy was successful because I can't deliver messages to you "
     text << "unprompted. That would actually be a terrible feature, imagine if "
-    text << "LinkedIn could send voice messages to your living room."
+    text << "LinkedIn could send voice messages to your living room. You'd never "
+    text << "stop hearing about how you're getting noticed."
   end
 end
