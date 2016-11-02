6c3c34a04b8d32f215b47f9854a4cffd1c880317
Jonan Scheffler <jonan@heroku.com>
2016-11-02 07:06:01 -0700

    Alexa was sending named arguments unnecessarily. Not so smart are ya Alexa?

--- app/models/alexa_response.rb
+++ app/models/alexa_response.rb
@@ -5,10 +5,7 @@ class AlexaResponse
     case payload.dig(:request, :intent, :name).downcase
     when "deploy"
       @intent = Intents::Deploy.new(payload: payload)
-      DeploymentJob.perform_later(
-        application: intent.application,
-        environment: intent.environment
-      )
+      DeploymentJob.perform_later(intent.application, intent.environment)
     end
   end
 
