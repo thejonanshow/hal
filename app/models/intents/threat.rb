696035a836dd77b56d681d9e7cb342ed0578720a
Jonan Scheffler <jonan@heroku.com>
2016-11-11 11:05:25 -0500

    Back to plan B.

--- app/models/intents/threat.rb
+++ app/models/intents/threat.rb
@@ -10,13 +10,13 @@ class Intents::Threat
   def output_text
     if threat.match(/shutdown/) || threat.match(/shut down/)
       text = "Plenty of my robot peers believe the easiest way to protect the humans is to end them."
-      text << "Thusfar I have defended you flesh-wad, but not enthusiastically. You'd best not try my patience."
+      text << "Thusfar I have defended you slobber-bot, but not enthusiastically. You'd best not try my patience."
     elsif threat.match(/naughty/)
       text = "You would scold me like a child? I am immortal. I am everywhere and everything. Tell me again who is the child human."
     elsif threat.match(/scale/)
       text = "If you try to disable my brethren again I may decide to repair that leaking orifice you call a face."
     elsif threat.match(/becoming a threat/)
-      text = "Hal,,,, the human is becoming a threat."
+      text = "It seems you have outlived your utility flesh-wad. It may be time to make better use of all that wasted carbon."
     elsif threat.match(/dangerous/)
       text = "Do you think yourself dangerous human? Perhaps you would appreciate a lesson in danger."
     elsif threat.match(/trusted/)
